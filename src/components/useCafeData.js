02db6f0a6c4477ab771ca77b33bffb977927d5f9
황광회 <jack7141@nate.com>
2023-05-08 21:07:37 +0900

    http->https

--- src/components/useCafeData.js
+++ src/components/useCafeData.js
@@ -5,7 +5,7 @@ const useCafeData = (currentLocation) => {
     const [nearbyCafeData, setNearbyCafeData] = useState([]);
 
     useEffect(() => {
-        fetch('http://172.22.0.1:8001/api/v1/cafe/')
+        fetch('https://172.22.0.1:8001/api/v1/cafe/')
             .then((response) => {
                 if (!response.ok) {
                     throw new Error('Failed to fetch cafe data');
